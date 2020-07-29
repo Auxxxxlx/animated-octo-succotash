c5aba9ba1e5364fb31cd579573cce331476ec9c1
Alecyrus <alecyrus@163.com>
2020-07-29 11:00:35 +0800

    fix: 🐛 init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import G6 from '@antv/g6'
+
+console.log(G6)
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup () {
+  }
+}
+</script>
